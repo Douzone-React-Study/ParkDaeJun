7abee3f1cd110d504bb34b230762fa72fa5e2587
pt807 <pt807@naver.com>
2022-12-30 14:12:45 +0900

    콜라츠 추측

---
+++ javascript-study/javascript03/prob04.js
@@ -0,0 +1,13 @@
+/** 콜라츠 추측*/
+const solution = (num) => {
+  let count = 0;
+  while (num != 1 && count <= 500) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    count++;
+  }
+  return num === 1 ? count : -1;
+};
